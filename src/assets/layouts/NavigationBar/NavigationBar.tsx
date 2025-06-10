ae2685dbcc5dd6c692396434e8d32716b04f14c3
Jonathan Kuek <75208150+kyhjonathan@users.noreply.github.com>
2025-06-10 15:29:11 +0200

    change sign in button to icon

--- src/assets/layouts/NavigationBar/NavigationBar.tsx
+++ src/assets/layouts/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,8 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import "./NavigationBar.css";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
 
 const NavigationBar = () => {
   const location = useLocation();
@@ -19,46 +20,59 @@ const NavigationBar = () => {
                 justifyContent: "space-evenly",
               }}
             >
-              <Button
-                className="nav-button"
-                component={Link}
-                to="/"
-                variant="contained"
-                color={location.pathname === "/" ? "primary" : "inherit"}
-                sx={{ borderRadius: "10px" }}
+              <div
+                style={{
+                  display: "flex",
+                  gap: "3rem",
+                  justifyContent: "center",
+                  flex: 1,
+                }}
               >
-                Home
-              </Button>
-              <Button
-                className="nav-button"
-                component={Link}
-                to="/about"
-                variant="contained"
-                color={location.pathname === "/about" ? "primary" : "inherit"}
-                sx={{ borderRadius: "10px" }}
-              >
-                About
-              </Button>
-              <Button
-                className="nav-button"
-                component={Link}
-                to="/contact"
-                variant="contained"
-                color={location.pathname === "/contact" ? "primary" : "inherit"}
-                sx={{ borderRadius: "10px" }}
-              >
-                Contact
-              </Button>
-              <Button
-                className="nav-button"
-                component={Link}
-                to="/signin"
-                variant="contained"
-                color={location.pathname === "/signin" ? "primary" : "inherit"}
-                sx={{ borderRadius: "10px" }}
-              >
-                Sign In
-              </Button>
+                <Button
+                  className="nav-button"
+                  component={Link}
+                  to="/"
+                  variant="contained"
+                  color={location.pathname === "/" ? "primary" : "inherit"}
+                  sx={{ borderRadius: "10px" }}
+                >
+                  Home
+                </Button>
+                <Button
+                  className="nav-button"
+                  component={Link}
+                  to="/about"
+                  variant="contained"
+                  color={location.pathname === "/about" ? "primary" : "inherit"}
+                  sx={{ borderRadius: "10px" }}
+                >
+                  About
+                </Button>
+                <Button
+                  className="nav-button"
+                  component={Link}
+                  to="/contact"
+                  variant="contained"
+                  color={
+                    location.pathname === "/contact" ? "primary" : "inherit"
+                  }
+                  sx={{ borderRadius: "10px" }}
+                >
+                  Contact
+                </Button>
+              </div>
+              <div style={{ marginLeft: "auto" }}>
+                <IconButton
+                  component={Link}
+                  to="/signin"
+                  color={
+                    location.pathname === "/signin" ? "primary" : "inherit"
+                  }
+                  size="medium"
+                >
+                  <AccountCircle sx={{ fontSize: 40 }} />
+                </IconButton>
+              </div>
             </Nav>
           </Navbar.Collapse>
         </Container>
